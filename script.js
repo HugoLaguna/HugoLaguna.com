dc7670ade3be71074b20216565f8dddaf0493a87
HugoLaguna <mkdrdre@gmail.com>
2018-03-05 08:26:40 -0500

    boton and mario

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 counter = 0;
 page = 0;
+animaciones = 0;
 function navPage(){
     if(page==0){
         page=1;
         counter=50;
-        document.getElementById('line').style.height='50%';      
+        document.getElementById('line').style.height='50%';
+        document.getElementById('mario').style.left='50%';
+      
         rePage();
     }else{
         page=0;
         counter=0;
-        document.getElementById('line').style.height='0%';      
+        document.getElementById('line').style.height='0%'; 
+        document.getElementById('mario').style.left='0%';     
         rePage();
     }
 }
@@ -27,8 +31,13 @@ function rePage(){
 document.addEventListener("DOMMouseScroll", function(event) {
 
     counter = counter + event.detail;
-    if (counter>=100){counter=0;}else if(counter<=0){counter=80;}
+    if (counter>=100){counter=0;}else if(counter<=0){counter=90;}
     document.getElementById('line').style.height=counter+'%';
+    document.getElementById('mario').style.left=counter+'%';
+    animaciones++;
+    document.getElementById('mario').style.animationIterationCount=animaciones
+    document.getElementById('mario').style.animationPlayState='running';
+
     if(counter>50){
         if(page==0){
             page=1;
@@ -45,8 +54,12 @@ document.addEventListener("scroll", function(event) {
     
         X =  document.getElementById('central')
         counter = counter + event.detail;
-        if (counter>=100){counter=0;}else if(counter<=0){counter=80;}
+        if (counter>=100){counter=0;}else if(counter<=0){counter=90;}
         document.getElementById('line').style.height=counter+'%';
+        document.getElementById('mario').style.left=counter+'%';
+        animaciones++;
+        document.getElementById('mario').style.animationIterationCount=animaciones
+        document.getElementById('mario').style.animationPlayState='running';
         if(counter>50){
             if(page==0){
                 page=1;
